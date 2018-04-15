f28c17e2200aa919a38317ab1babd6e9f4bda9c5
yajac <yajac.com@gmail.com>
2018-04-15 10:25:10 -0400

    Clean up unit test

--- src/main/test/org/yajac/reddit/lambda/RedditCacheReadHandlerTest.java
+++ src/main/test/org/yajac/reddit/lambda/RedditCacheReadHandlerTest.java
@@ -2,17 +2,18 @@ package org.yajac.reddit.lambda;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.yajac.reddit.BaseTestClass;
 
 import java.util.Collection;
 
-public class RedditCacheReadHandlerTest {
+public class RedditCacheReadHandlerTest extends BaseTestClass {
 
     @Test
     public void handleRequest() throws Exception {
         RedditCacheReadHandler handler = new RedditCacheReadHandler();
         CacheRequest request = new CacheRequest();
-        request.setSubtopic("new");
-        CacheResponse response = handler.handleRequest(request, null);
+        request.setSubtopic("test");
+        CacheResponse response = handler.handleRequest(request, getContext());
         Assert.assertNotNull(response);
         Collection<String> bodies = response.getBody();
         Assert.assertTrue(bodies.size() >= 10);
